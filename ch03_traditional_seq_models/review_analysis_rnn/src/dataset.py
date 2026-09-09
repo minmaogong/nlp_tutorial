22ff650f308f539c937d5d9125a296c301858d9a
公茂民 <maomin.gong@eeoa.com>
2026-09-09 16:57:18 +0800

    new: RNN案例-AI智评-构建数据集和加载器

---
+++ ch03_traditional_seq_models/review_analysis_rnn/src/dataset.py
@@ -0,0 +1,48 @@
+import torch
+import pandas as pd
+from torch.utils.data import Dataset, DataLoader
+
+# 自定义数据集类
+class ReviewAnalysisDataset(Dataset):
+    # 初始化
+    def __init__(self, file_path):
+        self.data = pd.read_json(file_path, lines=True).to_dict(orient='records')
+
+    # 获取长度
+    def __len__(self):
+        return len(self.data)
+
+    # 获取元素
+    def __getitem__(self, idx):
+        input = torch.tensor(self.data[idx]['review'])
+        target = torch.tensor(self.data[idx]['label'], dtype=torch.float)
+        return input, target
+
+# 获取DataLoader
+def get_dataloader(train=True):
+    # 根据参数判断获取哪个数据集
+    file_path = '../data/processed/' + ('train.jsonl' if train else 'test.jsonl')
+    # 创建数据集
+    dataset = ReviewAnalysisDataset(file_path)
+    # 创建加载器
+    dataloader = DataLoader(dataset, batch_size=64, shuffle=True)
+    return dataloader
+
+
+if __name__ == '__main__':
+    # 测试数据集
+    train_dataset = ReviewAnalysisDataset('../data/processed/train.jsonl')
+    print(len(train_dataset))
+    print(train_dataset[0])
+
+    # 测试数据加载器
+    train_loader = get_dataloader(train=True)
+    test_loader = get_dataloader(train=False)
+    # 取一批数据
+    for input, target in train_loader:
+        print(input.shape, target.shape)
+        break
+    test_iter = iter(test_loader)
+    input, target = next(test_iter)
+    print(input.shape, target.shape)
+
