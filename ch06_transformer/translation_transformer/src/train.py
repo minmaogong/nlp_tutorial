29e73894786fc1c46960df701369a9d94d9fba76
maomin <1220826363@qq.com>
2026-09-25 00:27:56 +0800

    new: Transformer案例-中英翻译3.0-模型训练

--- ch06_transformer/translation_transformer/src/train.py
+++ ch06_transformer/translation_transformer/src/train.py
@@ -7,7 +7,7 @@ from torch.utils.tensorboard import SummaryWriter
 
 from config import *
 from dataset import get_dataloader
-from model import TranslationSeq2SeqModel
+from model import TranslationModel
 from tokenizer import ChineseTokenizer, EnglishTokenizer
 
 # 训练一个轮次
@@ -17,20 +17,22 @@ def train_one_epoch(model, dataloader, loss_fn, optimizer, device):
     # 按批次进行迭代
     for inputs, targets in tqdm(dataloader, desc="Training"):
         inputs, targets = inputs.to(device), targets.to(device)
-        # 前向传播
-        # 1. 编码
-        encode_outputs, context_vector = model.encoder(inputs)
-        # 2. 解码
-        # 基于targets 得到解码器的真实输入和目标
+        # 1. 前向传播
+        # 1.1 基于targets 得到解码器的真实输入和目标
         decoder_inputs = targets[:, :-1] # (N, L) => [['sos', xxxx, xxxx, xxx, xxx], ['sos', xxxx, xxxx, xxx, 'eos'], ['sos', xxxx, 'eos', 0, 0]]
         decoder_targets = targets[:, 1:] # (N, L) => [[xxxx, xxxx, xxx, xxx, 'eos'], [xxxx, xxxx, xxx, 'eos', 0], [xxxx, 'eos', 0, 0, 0]]
-        # 解码器前向传播
-        decoder_outputs, _ = model.decoder(decoder_inputs, context_vector.unsqueeze(0), encode_outputs)
+
+        # 1.2 准备掩码：源序列padding掩码，目标序列源码
+        src_key_padding_mask = (inputs == model.src_embedding.padding_idx)
+        tgt_mask = model.transformer.generate_square_subsequent_mask(decoder_inputs.shape[1])
+
+        # 1.3 前向传播
+        output = model(src=inputs, tgt=decoder_inputs, src_key_padding_mask=src_key_padding_mask, tgt_mask=tgt_mask)
 
         # 计算损失
         # 将解码输出转换为 (N, C=vocab_size, L)
-        decoder_outputs = decoder_outputs.transpose(1, 2) # 后两个维度交换，也可以使用permute 重拍维度顺序 permute(0, 2, 1) => (N, L, C) => (N, C, L)
-        loss = loss_fn(decoder_outputs, decoder_targets)
+        output = output.transpose(1, 2) # 后两个维度交换，也可以使用permute 重拍维度顺序 permute(0, 2, 1) => (N, L, C) => (N, C, L)
+        loss = loss_fn(output, decoder_targets)
         # 反向传播
         loss.backward()
         # 更新参数，计算梯度
@@ -61,10 +63,10 @@ def train():
     # word2id = { word:id for id, word in enumerate(id2word) }
 
     # 4. 创建模型
-    model = TranslationSeq2SeqModel(src_vocab_size=zh_tokenizer.vocab_size, tgt_vocab_size=en_tokenizer.vocab_size, src_padding_idx=zh_tokenizer.pad_id, tgt_padding_idx=en_tokenizer.pad_id).to(device)
+    model = TranslationModel(src_vocab_size=zh_tokenizer.vocab_size, tgt_vocab_size=en_tokenizer.vocab_size, src_padding_idx=zh_tokenizer.pad_id, tgt_padding_idx=en_tokenizer.pad_id).to(device)
 
     # 5. 损失函数
-    loss_fn = nn.CrossEntropyLoss(ignore_index=en_tokenizer.pad_id)
+    loss_fn = nn.CrossEntropyLoss(ignore_index=en_tokenizer.pad_id) # ignore_index的作用是告诉损失函数在计算损失时忽略掉那些被标记为padding的词的损失，因为这些词不应该对模型的训练产生影响。
 
     # 6. 优化器
     optimizer = optim.Adam(model.parameters(), lr=LEARNING_RATE)
